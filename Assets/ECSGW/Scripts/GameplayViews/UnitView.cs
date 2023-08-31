7574d899bc32d9d3f5dcd6231702f9b9738fc346
Nashet <not@mmm.com>
2023-08-31 11:42:00 +0200

    Add flags to units

--- Assets/ECSGW/Scripts/GameplayViews/UnitView.cs
+++ Assets/ECSGW/Scripts/GameplayViews/UnitView.cs
@@ -1,7 +1,23 @@
-﻿using UnityEngine;
+﻿using Nashet.FlagGeneration;
+using UnityEngine;
 
 namespace Nashet.GameplayView
 {
 	public class UnitView : MonoBehaviour
-	{ }
+	{
+		[SerializeField]
+		private SpriteRenderer sprite;
+
+		public void SetFlag(Sprite sprite)
+		{
+			this.sprite.sprite = sprite;
+		}
+
+		private void Start()
+		{
+			var texture = FlagGenerator.Generate(128, 128);
+			Sprite sprite2 = Sprite.Create(texture, new Rect(0, 0, 128, 128), new Vector2(1f, 1f));
+			SetFlag(sprite2);
+		}
+	}
 }
